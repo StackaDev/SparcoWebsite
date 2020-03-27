60ad2e7803134f3182228fc81fad4ec2fd83140e
HABEEBRAHIMANP <habeebmeladi@gmail.com>
2020-03-27 17:38:53 +0530

    aranged

--- Ref/Sparcot_Web-master/next.config.js
+++
@@ -1,5 +0,0 @@
-const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
-const withImages = require('next-images');
-
-module.exports = withImages(withCSS(withSass()));
